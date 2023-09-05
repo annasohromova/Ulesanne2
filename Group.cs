86f1a59edb093a116398ca6d8e57b71c79c3e727
opilane <opilane@HP-CZC2349HV2>
2023-09-05 16:23:40 +0300

    Add project files.

---
+++ Group.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulesanne2
+{
+
+    public class Group
+    {
+        public List<Member> Members { get; } = new List<Member>();
+        private readonly int _maxAmount;
+
+        public Group(int maxAmount)
+        {
+            _maxAmount = maxAmount;
+        }
+
+        public bool AddMember(string name, int age)
+        {
+            if (Members.Any(member => member.Name == name)) return false;
+            if (Members.Count >= _maxAmount) return false;
+            Members.Add(new Member { Name = name, Age = age });
+            return true;
+        }
+
+        public int GetMembersCount()
+        {
+            return Members.Count;
+        }
+
+        public bool HasMember(string name)
+        {
+            return Members.Any(member => member.Name == name);
+        }
+
+        public int GetOldestMemberAge()
+        {
+            if (Members.Count == 0) return 0; // Handle case when there are no members.
+            return Members.Max(member => member.Age);
+        }
+
+        public int GetYoungestMemberAge()
+        {
+            if (Members.Count == 0) return 0; // Handle case when there are no members.
+            return Members.Min(member => member.Age);
+        }
+    }
+
+    public class Member
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Ulesanne2.Group;
+
+namespace Ulesanne2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            {
+                Group group = new Group(3);
+                Console.WriteLine(AddMemberAndColor(group, "John"));  // True
+                Console.WriteLine(AddMemberAndColor(group, "John"));  // False
+                Console.WriteLine(AddMemberAndColor(group, "Mary"));  // True
+                Console.WriteLine(AddMemberAndColor(group, "Jane"));  // True
+                Console.WriteLine(AddMemberAndColor(group, "Joseph"));  // False
+                Console.WriteLine(string.Join(", ", group.Members));  // John, Mary, Jane
+                Console.WriteLine(group.GetMembersCount());  // 3
+
+                Group bookClub = new Group(4);
+                Console.WriteLine(AddMemberAndColor(bookClub, "Albert"));   // True
+                Console.WriteLine(AddMemberAndColor(bookClub, "Samantha"));  // True
+                Console.WriteLine(AddMemberAndColor(bookClub, "Robert"));   // True
+                Console.WriteLine(AddMemberAndColor(bookClub, "Roberta"));  // True
+                Console.WriteLine(string.Join(", ", bookClub.Members));  // Albert, Samantha, Robert, Roberta
+
+                Console.WriteLine(AddMemberAndColor(bookClub, "Albert"));  // True
+                Console.WriteLine(AddMemberAndColor(bookClub, "John"));  // False
+            }
+        }
+
+        private static string AddMemberAndColor(Group group, string member)
+        {
+            if (group.AddMember(member))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                return "True";
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                return "False";
+            }
+        }
+    }
+}
